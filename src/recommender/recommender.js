0a82d866245d2a93fcca3ba91b1ff78ea3060755
desertglacier <36528683+desertglacier@users.noreply.github.com>
2025-05-06 03:30:02 +0000

    Add comment

--- src/recommender/recommender.js
+++ src/recommender/recommender.js
@@ -40,18 +40,59 @@ function getAppropriatenessCode(userInput) {
     return 10 * responsiveness + durationNumber + 10;
   });
 
+  console.log('qwer.results', results)
+
   return Math.max(...results).toString();
 }
 
+/**
+ * The getRecommendation is the entry point into this file. 
+ * You can change the internals of the function, but ensure 
+ * the parameter and the return object have the same structure.
+ * 
+ * @param {{
+ *     pmhx: string | undefined;
+ *     meds: string | undefined; 
+ *     behaviors: {type: string, duration: string}[]
+ * }} userInput 
+ * Example:
+ *    {
+ *      pmhx: "Schizophrenia\nDementia\nDiabetes\nOsteoarthritis\nGout",
+ *      meds: "Metformin 500 mg BID\nAcetaminophen 650 mg QID\nClozapine 500 mg qHS\nLoxapine 5 mg qHS",
+ *      behaviors: [
+ *        {
+ *          type: "wandering",
+ *          duration: "ongoing"
+ *        },
+ *        {
+ *          type: "spitting",
+ *          duration: "ongoing"
+ *        },
+ *        {
+ *          type: "insomina",
+ *          duration: "ongoing"
+ *        },
+ *      ]
+ *    }
+ * 
+ * @returns {{ msg: string; danger: number}} recommendation
+ * Example:
+ *   {
+ *     msg: "Likely appropriate. Continue AP.",
+ *     danger: 4,
+ *   }
+ */
 export function getRecommendation(userInput) {
   const apCode = getApCode(userInput);
   const dxCode = getDxCode(userInput);
   const appropriatenessCode = getAppropriatenessCode(userInput);
 
+  console.log(apCode, dxCode, appropriatenessCode)
+
   return (
     CODE_RECOMMENDATIONS[apCode] ??
     CODE_RECOMMENDATIONS[dxCode] ??
     CODE_RECOMMENDATIONS[appropriatenessCode] ??
-    "ERROR"
+    { msg: "Encounted error generating a recommendation", danger: 1 }
   );
 }
